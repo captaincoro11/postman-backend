c4af2592f894f928d62658685ff4e853b12fef1e
captaincoro11 <captaincoro444@gmail.com>
2024-06-13 14:54:34 +0530

    non

--- api/index.js
+++ api/index.js
@@ -6,7 +6,12 @@ import cors from 'cors'
 
 dotenv.config();
 
-app.use(cors({}));
+const corsOptions = {
+       origin: '*',
+       optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+     };
+
+app.use(cors(corsOptions));
 
 const app = express();
 const port = process.env.PORT || 3000;
